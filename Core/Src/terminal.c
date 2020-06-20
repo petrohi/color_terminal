6cb4ac3d9ca1d91cb0624b18922e691cb1693d00
Peter Hizalev <peter@hizalev.com>
2020-06-19 22:51:39 -0700

    Handle main keyboard

--- Core/Src/terminal.c
+++ Core/Src/terminal.c
@@ -20,6 +20,7 @@
 enum keys_entry_type {
   IGNORE,
   CHR,
+  STR,
   HANDLER,
   ROUTER,
 };
@@ -33,6 +34,7 @@ struct keys_router {
 
 union keys_variant {
   character_t character;
+  const char *string;
   void (*handler)(struct terminal *);
   struct keys_router *router;
 };
@@ -42,8 +44,37 @@ struct keys_entry {
   union keys_variant data;
 };
 
+static void transmit_character(struct terminal *terminal,
+                               character_t character) {
+  memcpy(terminal->transmit_buffer, &character, 1);
+  terminal->callbacks->uart_transmit(terminal->transmit_buffer, 1);
+}
+
+static void transmit_string(struct terminal *terminal, const char *string) {
+  size_t len = strlen(string);
+  memcpy(terminal->transmit_buffer, string, len);
+  terminal->callbacks->uart_transmit(terminal->transmit_buffer, len);
+}
+
+#define PRINTF_BUFFER_SIZE 64
+
+static int transmit_printf(struct terminal *terminal, const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+
+  char buffer[PRINTF_BUFFER_SIZE];
+  int len = snprintf(buffer, PRINTF_BUFFER_SIZE, format, args);
+
+  va_end(args);
+  return len;
+}
+
 static size_t get_case(struct terminal *terminal) {
   return terminal->lock_state.caps ^ terminal->shift_state;
+}
+
+static size_t get_shift(struct terminal *terminal) {
+  return terminal->shift_state;
 }
 
 static void update_keyboard_leds(struct terminal *terminal) {
@@ -55,11 +86,20 @@ static void handle_caps_lock(struct terminal *terminal) {
   update_keyboard_leds(terminal);
 }
 
+static void handle_enter(struct terminal *terminal) {
+  transmit_character(terminal, '\r');
+  //receive_string("\r\n");
+}
+
 #define KEY_IGNORE                                                             \
   { IGNORE }
 #define KEY_CHR(c)                                                             \
   {                                                                            \
     CHR, { .character = c }                                                    \
+  }
+#define KEY_STR(s)                                                             \
+  {                                                                            \
+    STR, { .string = s }                                                       \
   }
 #define KEY_HANDLER(h)                                                         \
   {                                                                            \
@@ -75,89 +115,91 @@ static void handle_caps_lock(struct terminal *terminal) {
   }
 
 static const struct keys_entry *entries = (struct keys_entry[]){
-    KEY_IGNORE,                                       // NONE
-    KEY_IGNORE,                                       // ERRORROLLOVER
-    KEY_IGNORE,                                       // POSTFAIL
-    KEY_IGNORE,                                       // ERRORUNDEFINED
-    KEY_ROUTER(get_case, KEY_CHR('a'), KEY_CHR('A')), // A
-    KEY_IGNORE,                                       // B
-    KEY_IGNORE,                                       // C
-    KEY_IGNORE,                                       // D
-    KEY_IGNORE,                                       // E
-    KEY_IGNORE,                                       // F
-    KEY_IGNORE,                                       // G
-    KEY_IGNORE,                                       // H
-    KEY_IGNORE,                                       // I
-    KEY_IGNORE,                                       // J
-    KEY_IGNORE,                                       // K
-    KEY_IGNORE,                                       // L
-    KEY_IGNORE,                                       // M
-    KEY_IGNORE,                                       // N
-    KEY_IGNORE,                                       // O
-    KEY_IGNORE,                                       // P
-    KEY_IGNORE,                                       // Q
-    KEY_IGNORE,                                       // R
-    KEY_IGNORE,                                       // S
-    KEY_IGNORE,                                       // T
-    KEY_IGNORE,                                       // U
-    KEY_IGNORE,                                       // V
-    KEY_IGNORE,                                       // W
-    KEY_IGNORE,                                       // X
-    KEY_IGNORE,                                       // Y
-    KEY_IGNORE,                                       // Z
-    KEY_IGNORE,                                       // 1_EXCLAMATION_MARK
-    KEY_IGNORE,                                       // 2_AT
-    KEY_IGNORE,                                       // 3_NUMBER_SIGN
-    KEY_IGNORE,                                       // 4_DOLLAR
-    KEY_IGNORE,                                       // 5_PERCENT
-    KEY_IGNORE,                                       // 6_CARET
-    KEY_IGNORE,                                       // 7_AMPERSAND
-    KEY_IGNORE,                                       // 8_ASTERISK
-    KEY_IGNORE,                                       // 9_OPARENTHESIS
-    KEY_IGNORE,                                       // 0_CPARENTHESIS
-    KEY_IGNORE,                                       // ENTER
-    KEY_IGNORE,                                       // ESCAPE
-    KEY_IGNORE,                                       // BACKSPACE
-    KEY_IGNORE,                                       // TAB
-    KEY_IGNORE,                                       // SPACEBAR
-    KEY_IGNORE,                                       // MINUS_UNDERSCORE
-    KEY_IGNORE,                                       // EQUAL_PLUS
-    KEY_IGNORE,                                       // OBRACKET_AND_OBRACE
-    KEY_IGNORE,                                       // CBRACKET_AND_CBRACE
-    KEY_IGNORE,                                       // BACKSLASH_VERTICAL_BAR
-    KEY_IGNORE,                                       // NONUS_NUMBER_SIGN_TILDE
-    KEY_IGNORE,                                       // SEMICOLON_COLON
-    KEY_IGNORE,                                       // SINGLE_AND_DOUBLE_QUOTE
-    KEY_IGNORE,                                       // GRAVE_ACCENT_AND_TILDE
-    KEY_IGNORE,                                       // COMMA_AND_LESS
-    KEY_IGNORE,                                       // DOT_GREATER
-    KEY_IGNORE,                                       // SLASH_QUESTION
-    KEY_HANDLER(handle_caps_lock),                    // CAPS_LOCK
-    KEY_IGNORE,                                       // F1
-    KEY_IGNORE,                                       // F2
-    KEY_IGNORE,                                       // F3
-    KEY_IGNORE,                                       // F4
-    KEY_IGNORE,                                       // F5
-    KEY_IGNORE,                                       // F6
-    KEY_IGNORE,                                       // F7
-    KEY_IGNORE,                                       // F8
-    KEY_IGNORE,                                       // F9
-    KEY_IGNORE,                                       // F10
-    KEY_IGNORE,                                       // F11
-    KEY_IGNORE,                                       // F12
-    KEY_IGNORE,                                       // PRINTSCREEN
-    KEY_IGNORE,                                       // SCROLL_LOCK
-    KEY_IGNORE,                                       // PAUSE
-    KEY_IGNORE,                                       // INSERT
-    KEY_IGNORE,                                       // HOME
-    KEY_IGNORE,                                       // PAGEUP
-    KEY_IGNORE,                                       // DELETE
-    KEY_IGNORE,                                       // END1
-    KEY_IGNORE,                                       // PAGEDOWN
-    KEY_IGNORE,                                       // RIGHTARROW
-    KEY_IGNORE,                                       // LEFTARROW
-    KEY_IGNORE,                                       // DOWNARROW
-    KEY_IGNORE,                                       // UPARROW
+    KEY_IGNORE,                                        // NONE
+    KEY_IGNORE,                                        // ERRORROLLOVER
+    KEY_IGNORE,                                        // POSTFAIL
+    KEY_IGNORE,                                        // ERRORUNDEFINED
+    KEY_ROUTER(get_case, KEY_CHR('a'), KEY_CHR('A')),  // A
+    KEY_ROUTER(get_case, KEY_CHR('b'), KEY_CHR('B')),  // B
+    KEY_ROUTER(get_case, KEY_CHR('c'), KEY_CHR('C')),  // C
+    KEY_ROUTER(get_case, KEY_CHR('d'), KEY_CHR('D')),  // D
+    KEY_ROUTER(get_case, KEY_CHR('e'), KEY_CHR('E')),  // E
+    KEY_ROUTER(get_case, KEY_CHR('f'), KEY_CHR('F')),  // F
+    KEY_ROUTER(get_case, KEY_CHR('g'), KEY_CHR('G')),  // G
+    KEY_ROUTER(get_case, KEY_CHR('h'), KEY_CHR('H')),  // H
+    KEY_ROUTER(get_case, KEY_CHR('i'), KEY_CHR('I')),  // I
+    KEY_ROUTER(get_case, KEY_CHR('j'), KEY_CHR('J')),  // J
+    KEY_ROUTER(get_case, KEY_CHR('k'), KEY_CHR('K')),  // K
+    KEY_ROUTER(get_case, KEY_CHR('l'), KEY_CHR('L')),  // L
+    KEY_ROUTER(get_case, KEY_CHR('m'), KEY_CHR('M')),  // M
+    KEY_ROUTER(get_case, KEY_CHR('n'), KEY_CHR('N')),  // N
+    KEY_ROUTER(get_case, KEY_CHR('o'), KEY_CHR('O')),  // O
+    KEY_ROUTER(get_case, KEY_CHR('p'), KEY_CHR('P')),  // P
+    KEY_ROUTER(get_case, KEY_CHR('q'), KEY_CHR('Q')),  // Q
+    KEY_ROUTER(get_case, KEY_CHR('r'), KEY_CHR('R')),  // R
+    KEY_ROUTER(get_case, KEY_CHR('s'), KEY_CHR('S')),  // S
+    KEY_ROUTER(get_case, KEY_CHR('t'), KEY_CHR('T')),  // T
+    KEY_ROUTER(get_case, KEY_CHR('u'), KEY_CHR('U')),  // U
+    KEY_ROUTER(get_case, KEY_CHR('v'), KEY_CHR('V')),  // V
+    KEY_ROUTER(get_case, KEY_CHR('w'), KEY_CHR('W')),  // W
+    KEY_ROUTER(get_case, KEY_CHR('x'), KEY_CHR('X')),  // X
+    KEY_ROUTER(get_case, KEY_CHR('y'), KEY_CHR('Y')),  // Y
+    KEY_ROUTER(get_case, KEY_CHR('z'), KEY_CHR('Z')),  // Z
+    KEY_ROUTER(get_shift, KEY_CHR('1'), KEY_CHR('!')), // 1_EXCLAMATION_MARK
+    KEY_ROUTER(get_shift, KEY_CHR('2'), KEY_CHR('@')), // 2_AT
+    KEY_ROUTER(get_shift, KEY_CHR('3'), KEY_CHR('#')), // 3_NUMBER_SIGN
+    KEY_ROUTER(get_shift, KEY_CHR('4'), KEY_CHR('$')), // 4_DOLLAR
+    KEY_ROUTER(get_shift, KEY_CHR('5'), KEY_CHR('%')), // 5_PERCENT
+    KEY_ROUTER(get_shift, KEY_CHR('6'), KEY_CHR('^')), // 6_CARET
+    KEY_ROUTER(get_shift, KEY_CHR('7'), KEY_CHR('&')), // 7_AMPERSAND
+    KEY_ROUTER(get_shift, KEY_CHR('8'), KEY_CHR('*')), // 8_ASTERISK
+    KEY_ROUTER(get_shift, KEY_CHR('9'), KEY_CHR('(')), // 9_OPARENTHESIS
+    KEY_ROUTER(get_shift, KEY_CHR('0'), KEY_CHR(')')), // 0_CPARENTHESIS
+    KEY_HANDLER(handle_enter),                         // ENTER
+    KEY_CHR('\33'),                                    // ESCAPE
+    KEY_CHR('\10'),                                    // BACKSPACE
+    KEY_CHR('\11'),                                    // TAB
+    KEY_CHR(' '),                                      // SPACEBAR
+    KEY_ROUTER(get_shift, KEY_CHR('-'), KEY_CHR('_')), // MINUS_UNDERSCORE
+    KEY_ROUTER(get_shift, KEY_CHR('='), KEY_CHR('+')), // EQUAL_PLUS
+    KEY_ROUTER(get_shift, KEY_CHR('['), KEY_CHR('{')), // OBRACKET_AND_OBRACE
+    KEY_ROUTER(get_shift, KEY_CHR(']'), KEY_CHR('}')), // CBRACKET_AND_CBRACE
+    KEY_ROUTER(get_shift, KEY_CHR('\\'),
+               KEY_CHR('|')), // BACKSLASH_VERTICAL_BAR
+    KEY_IGNORE,               // NONUS_NUMBER_SIGN_TILDE
+    KEY_ROUTER(get_shift, KEY_CHR(';'), KEY_CHR(':')), // SEMICOLON_COLON
+    KEY_ROUTER(get_shift, KEY_CHR('\''),
+               KEY_CHR('"')), // SINGLE_AND_DOUBLE_QUOTE
+    KEY_ROUTER(get_shift, KEY_CHR('`'), KEY_CHR('~')), // GRAVE_ACCENT_AND_TILDE
+    KEY_ROUTER(get_shift, KEY_CHR(','), KEY_CHR('<')), // COMMA_AND_LESS
+    KEY_ROUTER(get_shift, KEY_CHR('.'), KEY_CHR('>')), // DOT_GREATER
+    KEY_ROUTER(get_shift, KEY_CHR('/'), KEY_CHR('?')), // SLASH_QUESTION
+    KEY_HANDLER(handle_caps_lock),                     // CAPS_LOCK
+    KEY_STR("\033[11~"),                               // F1
+    KEY_STR("\033[12~"),                               // F2
+    KEY_STR("\033[13~"),                               // F3
+    KEY_STR("\033[14~"),                               // F4
+    KEY_STR("\033[15~"),                               // F5
+    KEY_STR("\033[17~"),                               // F6
+    KEY_STR("\033[18~"),                               // F7
+    KEY_STR("\033[19~"),                               // F8
+    KEY_STR("\033[20~"),                               // F9
+    KEY_STR("\033[21~"),                               // F10
+    KEY_STR("\033[23~"),                               // F11
+    KEY_STR("\033[24~"),                               // F12
+    KEY_IGNORE,                                        // PRINTSCREEN
+    KEY_IGNORE,                                        // SCROLL_LOCK
+    KEY_IGNORE,                                        // PAUSE
+    KEY_IGNORE,                                        // INSERT
+    KEY_IGNORE,                                        // HOME
+    KEY_IGNORE,                                        // PAGEUP
+    KEY_IGNORE,                                        // DELETE
+    KEY_IGNORE,                                        // END1
+    KEY_IGNORE,                                        // PAGEDOWN
+    KEY_STR("\033[C"),                                 // RIGHTARROW
+    KEY_STR("\033[D"),                                 // LEFTARROW
+    KEY_STR("\033[B"),                                 // DOWNARROW
+    KEY_STR("\033[A"),                                 // UPARROW
     KEY_IGNORE, // KEYPAD_NUM_LOCK_AND_CLEAR
     KEY_IGNORE, // KEYPAD_SLASH
     KEY_IGNORE, // KEYPAD_ASTERIKS
@@ -177,31 +219,6 @@ static const struct keys_entry *entries = (struct keys_entry[]){
     KEY_IGNORE, // KEYPAD_DECIMAL_SEPARATOR_DELETE
 };
 
-static void transmit_character(struct terminal *terminal,
-                               character_t character) {
-  memcpy(terminal->transmit_buffer, &character, 1);
-  terminal->callbacks->uart_transmit(terminal->transmit_buffer, 1);
-}
-
-static void transmit_string(struct terminal *terminal, char *string) {
-  size_t len = strlen(string);
-  memcpy(terminal->transmit_buffer, string, len);
-  terminal->callbacks->uart_transmit(terminal->transmit_buffer, len);
-}
-
-#define PRINTF_BUFFER_SIZE 64
-
-static int transmit_printf(struct terminal *terminal, const char *format, ...) {
-  va_list args;
-  va_start(args, format);
-
-  char buffer[PRINTF_BUFFER_SIZE];
-  int len = snprintf(buffer, PRINTF_BUFFER_SIZE, format, args);
-
-  va_end(args);
-  return len;
-}
-
 static void handle_key(struct terminal *terminal,
                        const struct keys_entry *key) {
   struct keys_router *router;
@@ -211,6 +228,9 @@ static void handle_key(struct terminal *terminal,
     break;
   case CHR:
     transmit_character(terminal, key->data.character);
+    break;
+  case STR:
+    transmit_string(terminal, key->data.string);
     break;
   case HANDLER:
     key->data.handler(terminal);
@@ -316,6 +336,7 @@ static void clear_cols(struct terminal *terminal, size_t row, size_t from_col,
 }
 
 static void line_feed(struct terminal *terminal, int16_t rows) {
+  clear_cursor(terminal);
 
   if (terminal->cursor_row >= ROWS - rows) {
     scroll(terminal, SCROLL_UP, 0, ROWS,
@@ -837,7 +858,7 @@ static const receive_table_t hash_receive_table = {
     RECEIVE_HANDLER('8', receive_decaln),
 };
 
-static void receive_string(struct terminal *terminal, char *string) {
+static void receive_string(struct terminal *terminal, const char *string) {
   while (*string) {
     receive_character(terminal, *string);
     string++;
