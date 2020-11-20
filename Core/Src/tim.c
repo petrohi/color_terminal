3fc8bfbab8f1068bc3f0ee2104ed5bf95c973593
Peter Hizalev <peter@hizalev.com>
2020-11-19 19:33:19 -0800

    Adjust TIM1 prescaler for higher APB2 clock

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -33,7 +33,7 @@ void MX_TIM1_Init(void)
   TIM_MasterConfigTypeDef sMasterConfig = {0};
 
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 7200;
+  htim1.Init.Prescaler = 14400;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 10;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
